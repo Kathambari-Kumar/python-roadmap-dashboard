7e01ba9386f1615cbe27738b1d44ad27bee02fda
Kathambari Indrajith <sanika1250@gmail.com>
2025-11-03 22:10:44 +0100

    make header visible

--- python_roadmap.py
+++ python_roadmap.py
@@ -5,8 +5,8 @@ import streamlit as st
 # Create horizontal layout
 hide_streamlit_style = """
     <style>
+     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
-    header {visibility: hidden;}
     </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
